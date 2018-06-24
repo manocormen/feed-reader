b4b1effd39fc45b6127fc03cd3b06b193cec1202
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-06-24 11:25:14 +0200

    Add gulp task to compile sass into css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
 // Import modules
-const gulp = require('gulp');
+const gulp = require('gulp'); // To export tasks
+const sass = require('gulp-sass'); // To compile sass into css
+
+// Initialize variables
+const paths = {
+  styles: {
+    src: 'sass/**/*.scss',
+    dest: 'css/'
+  }
+};
 
 // Export tasks
 gulp.task('default', defaultTask)
+gulp.task('compileStyles', compileStyles)
 
 // Declare Tasks
+
+/* Executes default tasks */
 function defaultTask (done) {
   console.log('The defaultTask works!');
+  done(); // Signal completion of the execution of the function
+}
+
+/* Compile sass into css */
+function compileStyles (done) {
+  gulp.src(paths.styles.src) // Get sass files from sass directory
+      .pipe(sass().on('error', sass.logError)) // Compile them into css and log any errors
+      .pipe(gulp.dest(paths.styles.dest)); // Save them in css directory
   done(); // Signal completion of the execution of the function
 }
