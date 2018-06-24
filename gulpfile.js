ab16e565948b72f9f752bad1907afe2e46d29626
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-06-24 11:51:38 +0200

    Add autoprefixer to gulp's styles compilation task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Import modules
 const gulp = require('gulp'); // To export tasks
 const sass = require('gulp-sass'); // To compile sass into css
+const autoprefixer = require('gulp-autoprefixer'); // To add vendor prefixes to css
 
 // Initialize variables
 const paths = {
@@ -26,6 +27,9 @@ function defaultTask (done) {
 function compileStyles (done) {
   gulp.src(paths.styles.src) // Get sass files from sass directory
       .pipe(sass().on('error', sass.logError)) // Compile them into css and log any errors
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      })) // Autoprefixe them with browser vendor prefixes
       .pipe(gulp.dest(paths.styles.dest)); // Save them in css directory
   done(); // Signal completion of the execution of the function
 }
