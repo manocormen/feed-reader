2e05fc16d013eca2e4941dc139265dfd46e0eb7b
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-06-24 13:31:13 +0200

    Set up Browsersync in gulp for live editing

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 const gulp = require('gulp'); // To export tasks
 const sass = require('gulp-sass'); // To compile sass into css
 const autoprefixer = require('gulp-autoprefixer'); // To add vendor prefixes to css
+const browserSync = require('browser-sync').create(); // To edit live
 
 // Initialize variables
 const paths = {
@@ -12,30 +13,30 @@ const paths = {
 };
 
 // Export tasks
-gulp.task('default', defaultTask);
-gulp.task('compileStyles', compileStyles);
-gulp.task('watchChanges', watchChanges);
+gulp.task('default', serve);
+gulp.task('compile', compile);
 
 // Declare Tasks
 
-/* Executes default tasks */
-function defaultTask (done) {
-  console.log('The defaultTask works!');
+/* Initialize server and watch for changes */
+function serve (done) {
+  // Initialize browserSync server
+  browserSync.init({
+    server: "./" // Define directory to serve (should contain index.html)
+  });
+  gulp.watch(paths.styles.src, compile); // Watch for sass changes and compile
+  gulp.watch("./*html").on('change', browserSync.reload); // Watch for html changes and reload
   done(); // Signal completion of the execution of the function
 }
 
 /* Compile sass into css */
-function compileStyles (done) {
+function compile (done) {
   gulp.src(paths.styles.src) // Get sass files from sass directory
       .pipe(sass().on('error', sass.logError)) // Compile them into css and log any errors
       .pipe(autoprefixer({
         browsers: ['last 2 versions']
       })) // Autoprefixe them with browser vendor prefixes
-      .pipe(gulp.dest(paths.styles.dest)); // Save them in css directory
+      .pipe(gulp.dest(paths.styles.dest)) // Save them in css directory
+      .pipe(browserSync.stream()); // Stream to browser (ie upload it)
   done(); // Signal completion of the execution of the function
-}
-
-/* Watch for changes and trigger appropriate task in response */
-function watchChanges (done) {
-  gulp.watch(paths.styles.src, compileStyles); // Recompile styles on changes
 }
